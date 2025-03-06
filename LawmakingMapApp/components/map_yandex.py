31ac20daccee7cba50f8770bb251f9ed32bbaa0a
GeorgeWinner <Life.Winner@yandex.ru>
2025-03-06 23:27:41 +0300

    Draws map and some interface

--- LawmakingMapApp/components/map_yandex.py
+++ LawmakingMapApp/components/map_yandex.py
@@ -1,16 +1,9 @@
 from django_unicorn.components import UnicornView
 from django import forms
 
-class MapYandexForm(forms.Form):
-    task = forms.CharField(min_length=2, max_length=20, required=True)
 
 class MapYandexView(UnicornView):
-    form_class = MapYandexForm
 
-    task = ""
-    tasks = []
 
-    def add(self):
-        if self.is_valid():
-            self.tasks.append(self.task)
-            self.task = ""
+    def setMarker(self):
+        self.call("addMarker")
